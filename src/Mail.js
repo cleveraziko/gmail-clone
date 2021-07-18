0b0212d8e7ba384dfb6e11657baa412c244aec06
cleveraziko <74561277+cleveraziko@users.noreply.github.com>
2021-07-18 13:12:01 +0500

    .....

--- src/Mail.js
+++ src/Mail.js
@@ -1,10 +1,78 @@
+import { IconButton } from "@material-ui/core";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import React from "react";
 import "./Mail.css";
+import MoveToInboxIcon from "@material-ui/icons/MoveToInbox";
+import ErrorIcon from "@material-ui/icons/Error";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EmailIcon from "@material-ui/icons/Email";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { useHistory } from "react-router-dom";
+import PrintIcon from "@material-ui/icons/Print";
+import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { LabelImportant } from "@material-ui/icons";
 
 function Mail() {
+  const history = useHistory();
   return (
     <div className="mail">
-      <h1>Hello</h1>
+      <div className="mail__tools">
+        <div className="mail__toolsLeft">
+          <IconButton onClick={() => history.push("/")}>
+            <ArrowBackIcon />
+          </IconButton>
+          <IconButton>
+            <MoveToInboxIcon />
+          </IconButton>
+          <IconButton>
+            <ErrorIcon />
+          </IconButton>
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+          <IconButton>
+            <EmailIcon />
+          </IconButton>
+          <IconButton>
+            <WatchLaterIcon />
+          </IconButton>
+          <IconButton>
+            <CheckCircleIcon />
+          </IconButton>
+          <IconButton>
+            <LabelImportantIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="mail__toolsRight">
+          <IconButton>
+            <UnfoldMoreIcon />
+          </IconButton>
+          <IconButton>
+            <PrintIcon />
+          </IconButton>
+          <IconButton>
+            <ExitToAppIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="mail__body">
+        <div className="mail__bodyHeader">
+          <h2>Subject</h2>
+          <LabelImportantIcon className="mail__important" />
+          <p>title</p>
+          <p className="mail__time">time</p>
+        </div>
+        <div className="mail__message">
+          <p>wassssup !!!</p>
+        </div>
+      </div>
     </div>
   );
 }
